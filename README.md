97283994112fe85ebe3037e800599d59f99191b1
annabelka333 <139398794+annabelka333@users.noreply.github.com>
2024-12-14 00:29:19 +0200

    Added copyclipboard button and changed description of the weather app.Added a new image to the weather app

--- README.md
+++ README.md
@@ -6,3 +6,52 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+### Git commands 
+
+Add all changes
+```
+git add .
+```
+
+Write commiit
+
+```
+git commit -m "My changes that I made"
+```
+
+Working alone skip this step
+
+Download remote code
+```
+git pull
+```
+
+Send your changes to gitGub
+```
+git push origin -u <branch-name>
+```
+
+## More commands
+
+Change branch
+```
+git checkout <branch-name>
+```
+
+Create new branch
+```
+git checkout -b <branch-name>
+```
+
+Remove branch
+```
+git branch -D <branch-name>
+```
+
+Check current branch
+```
+git branch
+```
+
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,6 +1,8 @@
 import CardProject from "../components/card-project";
 import ContactBtn from "../components/contact-btn";
+import ClipboardButton from "../components/copyclipboard";
 
+const email = 'annabelka333@gmail.com';
 const projects = [
   {
     id: 1,
@@ -13,8 +15,8 @@ const projects = [
     id: 2,
     title: "Weather App",
     description:
-      "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by",
-    image: "images/weather.jpg",
+      "Built with a  responsive user interface.Integrated an external API to fetch real-time weather data based on   user input. Manage asynchronous API request and handeled errors to ensure smooth functionality. Focused on creating a clean and responsive UI for optimal user experience. ",
+    image: "images/weatherApp.webp",
   },
   {
     id: 3,
@@ -28,17 +30,28 @@ const projects = [
 //https://www.shecodes.io/graduates/79924-anna-vediashkyna
 
 const Home = () => {
+  // const handleCopyToClipboard = () => {
+  //   const textToCopy = "annabelka333@gmail.com";
+  //   navigator.clipboard.writeText(textToCopy)
+  //     .then(() => alert("Copied to clipboard."))
+  //     .catch((err) => console.error("Failed to copy text: ", err))
+  // };
   return (
     <>
-      <div className="hero bg-violet-50 text-center px-5 py-24">
+      <div className="bg-violet-50 text-center px-5 py-24">
         <p className="font-bold text-4xl">👋Hello, I am</p>
         <h1>Anna Vediashkyna</h1>
-        <p className="font-custom mb-12 text-2xl">Front-end developer based in Spain</p>
+        <p className="font-bold mb-12 text-3xl">Front-end developer</p>
         <ContactBtn />
+        <p className="mt-4 flex flex-row gap-4 justify-center itmes-center mb-12 text-2xl">
+          <a href={`mailto:${email}`}
+            className="">{email}</a>
+          <ClipboardButton copy={email} />
+        </p>
       </div>
       <div className="container mx-auto mb-16">
-        <p className="text-center m-12 font-bold">
-          Featured Project which is my best project be choice be show
+        <p className="font-serif text-2xl text-center m-12 font-bold ">
+          Featured Project: My best project, chosen by me, to showcase.
         </p>
         {projects.map((project) => (
           <CardProject
